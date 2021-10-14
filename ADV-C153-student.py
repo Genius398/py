d8bc180ec2d5175f4f6ea5aa99a81e0747ab380c
Anantjit <anantjitchander@gmail.com>
2021-10-14 16:34:26 +0530

    Update ADV-C153-student.py

--- ADV-C153-student.py
+++ ADV-C153-student.py
@@ -1,18 +1,18 @@
- import random *
  from tkinter import *
+ import random
  root=Tk()
  root.title("Array Dimentions")
  
  root.geometry("400x400")
  label = Label(root)
  
- array_3d =[[[1,2,3,4,5,6],["Hard","tall"],["A","B","C","G","S","J","T","R"]]]
+ array_3d =[[[1,2,3,4,5,6],["Hard","tall","shoft","shelf"],["A","B","C","G","S","J","T","R"]]]
 
  print(array_3d[0][2][3])
  
  def newPassword():
      randomNumberOne = random.randint(0,5)
-     randomNumberTwo = random.randint(0,1)
+     randomNumberTwo = random.randint(0,3)
      randomNumberThree = random.randint(0,7)
      
      letterOne = str(array_3d[0][0][randomNumberOne])
